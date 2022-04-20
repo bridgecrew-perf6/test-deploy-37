c9402435190789b7076e2644eb8980bde61be0c2
Hien Hoang Minh(Fuji - PHP) <hien.hoang.minh@fujitechjsc.com>
2022-04-20 11:59:08 +0700

    up

---
+++ app/Http/Controllers/APi/CorsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\APi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CorsController extends Controller
+{
+    public function index(){
+        dd(1111143);
+    }
+}
